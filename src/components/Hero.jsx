d53c38d9c2267574d93af15c5ba50d89f2ee76eb
Bhanu Saisree Thanniru <bthannir@uncc.edu>
2025-06-12 17:14:52 -0400

    updated resume path

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import profilePic from '../assets/bsst.jpg'
 import { HERO_CONTENT} from "../constants"
 import { motion } from 'framer-motion'
-
+import { resume } from '../../src/assets/projects/Bhanu_Saisree_Thanniru.pdf'
 const containerVariants = {
     hidden: { opacity: 0, x: -100},
     visible: {
@@ -35,7 +35,7 @@ const Hero = () => {
                         <motion.h2 variants={childVariants} className='pb-2 text-4xl tracking-tighter lg:text-8xl'>Bhanu Saisree Thanniru</motion.h2>
                         <motion.span variants={childVariants} className='bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent'>Full Stack Developer</motion.span>
                         <motion.p variants={childVariants} className='my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter'>{HERO_CONTENT}</motion.p>
-                        <motion.a variants={childVariants} href='src/assets/projects/Bhanu_Saisree_Thanniru.pdf' target='_blank' rel='noopener noreferrer' download className='bg-white rounded-full p-4 text-sm text-stone-800 mb-10'>Download Resume</motion.a>
+                        <motion.a variants={childVariants} href={resume} target='_blank' rel='noopener noreferrer' download className='bg-white rounded-full p-4 text-sm text-stone-800 mb-10'>Download Resume</motion.a>
                     </motion.div>
                 </div>
             </div>
